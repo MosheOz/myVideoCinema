e826ee4bc713cfaa3814645d4f3c2217813d7252
Moshe Oz <45286909+MosheOz@users.noreply.github.com>
2019-09-25 23:58:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/myVideosCinemaApp"));
 
 app.listen(process.env.PORT || 8080);
 
 app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname + "/dist/myVideosCinemaApp"));
+    res.sendFile(path.join(__dirname + "/dist/myVideosCinemaApp/index.html"));
 });
 
 console.log("wer'e listening :)")
